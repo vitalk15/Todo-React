be76a40c7a809ba3afeaa77455dfa3e5d00bb38a
Vital <vital.15@mail.ru>
2025-12-17 00:18:32 +0300

    передаём данные через props

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,17 @@
-const TodoItem = () => {
+const TodoItem = (props) => {
+	const { className = '', id, title, isDone } = props
+
 	return (
-		<li className="todo__item todo-item">
-			<input className="todo-item__checkbox" id="task-1" type="checkbox" checked />
-			<label className="todo-item__label" htmlFor="task-1">
-				Task 1
+		<li className={`todo-item ${className}`}>
+			<input
+				className="todo-item__checkbox"
+				id={id}
+				type="checkbox"
+				checked={isDone}
+				readOnly
+			/>
+			<label className="todo-item__label" htmlFor={id}>
+				{title}
 			</label>
 			<button className="todo-item__delete-button" aria-label="Delete" title="Delete">
 				<svg
